67fbd5d27d1c4a9db50f6e69b0835c6396f88ce8
LXL <656739286@qq.com>
2020-03-15 20:46:51 +0800

    提交

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BackGroundManageSolution.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
